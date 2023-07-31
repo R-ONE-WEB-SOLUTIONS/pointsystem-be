eaa10e9c955482939c2b13c85244faa390cbae3b
Jim Lao <laojimmarklino@gmail.com>
2023-07-31 15:56:12 +0800

    added routes api

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\API\ClientController;
+use App\Http\Controllers\API\PreRegController;
 use App\Http\Controllers\API\AccountController;
 use App\Http\Controllers\API\BusinessController;
 use App\Http\Controllers\API\UserTypeController;
+use App\Http\Controllers\API\ClientTypeController;
 use App\Http\Controllers\API\TransactionController;
+use App\Http\Controllers\API\PointCalculationController;
 
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -36,8 +39,9 @@ Route::group(['middleware' => ['auth:sanctum']], function() {
     Route::post('/viewUsersByBID',[UserController::class, 'viewUsersByBID']);
 
     //Pre Reg
-
+    Route::apiResource('preReg', PreRegController::class);
     //Client Type
+    Route::apiResource('clientType', ClientTypeController::class);
 
     //Client
     Route::apiResource('clients', ClientController::class);
@@ -54,4 +58,5 @@ Route::group(['middleware' => ['auth:sanctum']], function() {
     Route::post('/rewardPoints', [TransactionController::class, 'rewardPoints']);
 
     //Point Calculation
+    Route::apiResource('pointCalculation', PointCalculationController::class);
 });
