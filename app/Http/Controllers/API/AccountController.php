66d60c85548f1fa110af5bbdd30d13bb07610b17
jerelroi <jerelroi@apollotech.co>
2023-08-06 20:43:14 +0800

    call accounts service

--- app/Http/Controllers/API/AccountController.php
+++ app/Http/Controllers/API/AccountController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Models\Account;
+use App\Services\AccountService;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -23,7 +24,10 @@ class AccountController extends Controller
         //
     }
 
-    
+    public function viewAllAccounts(Request $request)
+    {   
+        return $this->accountService->viewAllAccounts($request);
+    }
     public function show($id)
     {
         //
