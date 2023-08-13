12fe42e53885a964c394fc8c055f1db91f5527c1
jerelroi <jerelroi@apollotech.co>
2023-08-13 13:33:31 +0800

    include client type in returning transactions

--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -46,9 +46,10 @@ class TransactionController extends Controller
     {
         if($request->business_id != null){
 
-            $transactions = Transaction::select('transactions.*','clients.first_name','clients.middle_name','clients.last_name', 'businesses.id as business_id', 'businesses.business_name as business_name')
+            $transactions = Transaction::select('transactions.*','client_types.client_type','clients.first_name','clients.middle_name','clients.last_name', 'businesses.id as business_id', 'businesses.business_name as business_name')
                             ->join('accounts', 'transactions.account_id', '=', 'accounts.id')
                             ->join('clients', 'accounts.client_id', '=', 'clients.id')
+                            ->join('client_types', 'client_types.id', '=', 'clients.client_type_id')
                             ->join('businesses', 'clients.business_id', '=', 'businesses.id')
                             ->where('businesses.id', $request->business_id)
                             ->get();
@@ -61,9 +62,10 @@ class TransactionController extends Controller
             
 
         }else{
-            $transactions = Transaction::select('transactions.*', 'clients.first_name','clients.middle_name','clients.last_name','businesses.id as business_id', 'businesses.business_name as business_name')
+            $transactions = Transaction::select('transactions.*','client_types.client_type', 'clients.first_name','clients.middle_name','clients.last_name','businesses.id as business_id', 'businesses.business_name as business_name')
                             ->join('accounts', 'transactions.account_id', '=', 'accounts.id')
                             ->join('clients', 'accounts.client_id', '=', 'clients.id')
+                            ->join('client_types', 'client_types.id', '=', 'clients.client_type_id')
                             ->join('businesses', 'clients.business_id', '=', 'businesses.id')
                             ->get();
 
