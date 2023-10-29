8970d9d3ac638ec21af4c782850543370c7eb736
Roi Rotoras <jerelroi@apollotech.co>
2023-10-29 16:23:45 +0800

    zxc

--- app/Services/ClientService.php
+++ app/Services/ClientService.php
@@ -197,13 +197,14 @@ class ClientService {
 
 
         if ($expiryDate > $today) {
+
             // Client's expiry date is more than today, update the client's active status
             if ($client->client_type_id == 1 && $client->business_id == 1) {
-
-                $client->update(['active' => 1, 'expiry_date' => $client->expiry_date->addYears(3)->setTime(23, 59, 59)]);
+                $expiryDate->add(new DateInterval('P3Y'));
+                $client->update(['active' => 1, 'expiry_date' => $expiryDate]);
             } elseif ($client->client_type_id == 2 && $client->business_id == 1) {
-
-                $client->update(['active' => 1, 'expiry_date' => $client->expiry_date->addYears(6)->setTime(23, 59, 59)]);
+                $expiryDate->add(new DateInterval('P6Y'));
+                $client->update(['active' => 1, 'expiry_date' => $expiryDate]);
             }
 
 
